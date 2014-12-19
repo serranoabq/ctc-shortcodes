378bad2e29fa72a5e1be483a4e5ea633db17e271
serranoabq <serranoabq@gmail.com>
2014-12-18 21:16:22 -0700

    Add GitHub updater info to header

--- ctc-shortcodes.php
+++ ctc-shortcodes.php
@@ -4,6 +4,8 @@
     Description: Plugin to display Church Theme Content within your theme using shortcodes. Requires <strong>Church Theme Content</strong> plugin.
     Version: 1.0
     Author: Justin R. Serrano
+    GitHub Plugin URI: https://github.com/serranoabq/ctc-shortcodes
+    GitHub Branch:     master
 */
 
 // No direct access
